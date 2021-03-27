4f40d8c7018acac298e6027a2e7b8e55a1f6d29f
Johanna Cano <cano0029@algonquinlive.com>
2021-03-27 09:57:57 -0400

    Add search button

--- js/app.js
+++ js/app.js
@@ -1,8 +1,34 @@
+const IDB = (
+  (function init() {
+    let db = null;
+    let objectStore = null;
+    let DBOpenReq = indexedDB.open()
+  }) ()
+)
+
+const MovieDB = {
+  /*************
+        SAMPLE URLS
+        
+        1. To get the config data like image base urls
+        https://api.themoviedb.org/3/configuration?api_key=<apiKey>
+        
+        2. To fetch a list of movies based on a keyword
+        https://api.themoviedb.org/3/search/movie?api_key=<apiKey>&query=<keyword>
+        
+        3. To fetch more details about a movie
+        https://api.themoviedb.org/3/movie/<movie-id>?api_key=<apiKey>
+  *************/
+
+  apiKey: '8b315e48d59ed2c712994a028435c067',
+
+}
 
 const APP = {
   init(){
     APP.registerServiceWorker()
   },
+
   registerServiceWorker () {
     try {
       if('serviceWorker' in navigator) {
@@ -14,7 +40,10 @@ const APP = {
     } catch (error) {
       console.log('Service worker not registered', error)
     }
-  }
+  },
+
+
+
 }
 
 document.addEventListener('DOMContentLoaded', APP.init)
