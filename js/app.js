e8b961017a8bde6c83742195da2e68ef350e1a3f
Johanna Cano <cano0029@algonquinlive.com>
2021-04-01 15:44:23 -0400

    Format code with prettier

--- js/app.js
+++ js/app.js
@@ -1,7 +1,4 @@
-// TO DO: 
-// put html under pages at root
-
-const APP = {  
+const APP = {
   apiKey: '8b315e48d59ed2c712994a028435c067',
   baseURL: 'https://api.themoviedb.org/3/',
   imgURL: 'https://image.tmdb.org/t/p/',
@@ -12,18 +9,17 @@ const APP = {
   profile_sizes: ['w45', 'w185', 'h632', 'original'],
   still_sizes: ['w92', 'w185', 'w300', 'original'],
 
-  
   db: null,
   moviedbStore: null,
   dbVersion: 1,
-  
+
   isOnline: true,
   deferredInstall: null,
   isStandalone: false,
 
   init() {
     try {
-      if('serviceWorker' in navigator) {
+      if ('serviceWorker' in navigator) {
         window.addEventListener('load', (event) => {
           console.log('Service worker registered', event)
           navigator.serviceWorker.register('/serviceWorker.js')
@@ -36,14 +32,13 @@ const APP = {
     APP.openDB()
     APP.addListeners()
     APP.checkInstall()
-  }, 
-  
-  addListeners() {
+  },
 
+  addListeners() {
     window.addEventListener('online', () => {
       let message = {
         isOnline: true,
-        description: 'Connection back online'
+        description: 'Connection back online',
       }
       navigator.serviceWorker.controller.postMessage(message)
     })
@@ -51,7 +46,7 @@ const APP = {
     window.addEventListener('offline', () => {
       let message = {
         isOnline: false,
-        description: 'Connection lost. Offline.'
+        description: 'Connection lost. Offline.',
       }
       navigator.serviceWorker.controller.postMessage(message)
     })
@@ -59,40 +54,42 @@ const APP = {
     window.addEventListener('beforeinstallprompt', (event) => {
       event.preventDefault()
       APP.deferredInstall = event
-    });
+    })
 
     window.addEventListener('appinstalled', () => {
       let message = {
-        appInstalled: true
+        appInstalled: true,
       }
       navigator.serviceWorker.controller.postMessage(message)
     })
 
     let btnInstall = document.getElementById('btnInstall')
     btnInstall?.addEventListener('click', APP.startChromeInstall)
-    
-    document.getElementById('searchForm').addEventListener('submit', APP.handleFormSubmit)
+
+    document
+      .getElementById('searchForm')
+      .addEventListener('submit', APP.navigateResultsPage)
 
     let movies = document.querySelector('.movies')
-    if (movies) { 
-      movies.addEventListener('click', APP.navigateSuggestPage) 
+    if (movies) {
+      movies.addEventListener('click', APP.navigateSuggestPage)
     }
   },
-  
-  checkInstall () {
+
+  checkInstall() {
     if (navigator.standalone) {
       console.log('Launched Location: Installed (iOS)')
-      APP.isStandalone = true;
+      APP.isStandalone = true
     } else if (matchMedia('(display-mode: standalone)').matches) {
       console.log('Launch Location: Installed (PWA)')
-      APP.isStandalone = true;
+      APP.isStandalone = true
     } else {
       console.log('Launch Location: Browser Tab')
-      APP.isStandalone = false;
+      APP.isStandalone = false
     }
-  }, 
+  },
 
-  startChromeInstall () {
+  startChromeInstall() {
     if (APP.deferredInstall) {
       APP.deferredInstall.prompt()
       APP.deferredInstall.userChoice.then((choiceResult) => {
@@ -105,129 +102,139 @@ const APP = {
     }
   },
 
-  navigateSuggestPage (event) {
+  navigateSuggestPage(event) {
     event.preventDefault()
-    let anchor = event.target;
+    let anchor = event.target
     if (anchor.tagName === 'A') {
-      let card = anchor.closest('.card');
-      let title = card.querySelector('.card-title span').textContent;
-      let mid = card.getAttribute('data-id');
-      let base = location.origin;
-      let url = new URL('/suggestedMovies.html', base);
-      url.search = `?movie_id=${mid}&ref=${encodeURIComponent(title)}`;
-      location.href = url;
+      let card = anchor.closest('.card')
+      let title = card.querySelector('.card-title span').textContent
+      let mid = card.getAttribute('data-id')
+      let base = location.origin
+      let url = new URL('/suggestedMovies.html', base)
+      url.search = `?movie_id=${mid}&ref=${encodeURIComponent(title)}`
+      location.href = url
     }
   },
 
-  async handleFormSubmit(event) {
+  async navigateResultsPage(event) {
     event.preventDefault()
     const searchInput = await event.target.search.value
-    const keyword = searchInput.trim() 
+    const keyword = searchInput.trim()
 
-      if (keyword){
-        event.preventDefault()
-        let base = location.origin
-        let url = new URL('/searchResults.html', base) 
-        url.search = '?keyword=' + encodeURIComponent(keyword)
-        location.href = url  
-        APP.clearForm()
+    if (keyword) {
+      event.preventDefault()
+      let base = location.origin
+      let url = new URL('/searchResults.html', base)
+      url.search = '?keyword=' + encodeURIComponent(keyword)
+      location.href = url
+      APP.clearForm()
     }
   },
 
   pageLoaded() {
-    let params = new URL(document.location).searchParams;
-    let keyword = params.get('keyword');
-    
-    if (keyword) { APP.checkMovieStore(keyword) }
+    let params = new URL(document.location).searchParams
+    let keyword = params.get('keyword')
 
-    let id = parseInt(params.get('movie_id'));
-    let ref = params.get('ref');
+    if (keyword) {
+      APP.checkMovieStore(keyword)
+    }
+
+    let id = parseInt(params.get('movie_id'))
+    let ref = params.get('ref')
     if (id && ref) {
-      APP.checkSuggestStore({ id, ref });
+      APP.checkSuggestStore({ id, ref })
     }
   },
 
-  async checkMovieStore (keyword) {
+  async checkMovieStore(keyword) {
     let transaction = await APP.makeTransaction('movieStore', 'readonly')
     transaction.oncomplete = () => {
-      console.log('Task of looking in movieStore for already saved movie results is complete')
+      console.log(
+        'Task of looking in movieStore for already saved movie results is complete'
+      )
     }
     let store = transaction.objectStore('movieStore')
-    let getRequest = await store.getAll(keyword) 
+    let getRequest = await store.getAll(keyword)
 
     getRequest.onsuccess = (event) => {
       let request = event.target.result
       if (request.length === 0) {
         APP.getData(keyword)
       } else {
-        console.log(`Movie results found under "${keyword}" is already saved in movieStore`)
+        console.log(
+          `Movie results found under "${keyword}" is already saved in movieStore`
+        )
         let movies = request[0].results
         APP.buildList(movies)
-      }  
+      }
     }
   },
 
-  async checkSuggestStore ({ id, ref }) {
+  async checkSuggestStore({ id, ref }) {
     let transaction = await APP.makeTransaction('suggestStore', 'readonly')
 
     transaction.oncomplete = () => {
-      console.log('Task of looking in suggestStore for the movie id is complete')
+      console.log(
+        'Task of looking in suggestStore for the movie id is complete'
+      )
     }
     let store = transaction.objectStore('suggestStore')
-    let getRequest = await store.getAll(id) 
+    let getRequest = await store.getAll(id)
 
     getRequest.onsuccess = (event) => {
       let request = event.target.result
       if (request.length === 0) {
         APP.getSuggest({ id, ref })
       } else {
-        console.log(`Recommended movies under genre id "${id}" is already saved in the suggestStore`)
+        console.log(
+          `Recommended movies under genre id "${id}" is already saved in the suggestStore`
+        )
         let movies = request[0].results
         APP.buildList(movies)
-      }  
+      }
     }
   },
-  
-  async getData (keyword){
+
+  async getData(keyword) {
     APP.postResultsTitle(keyword)
     let url = `${APP.baseURL}search/movie?api_key=${APP.apiKey}&query=${keyword}`
-    const response = await fetch(url) 
+    const response = await fetch(url)
 
     try {
       if (!response.ok) throw new Error(response.message)
-        let movieResults = {
-          keyword, 
-          results: await response.json()
-        }
+      let movieResults = {
+        keyword,
+        results: await response.json(),
+      }
       return APP.saveResults(movieResults)
     } catch (error) {
-        console.warn('Could not fetch movies', error)
+      console.warn('Could not fetch movies', error)
     }
   },
 
   async getSuggest({ id, ref }) {
     APP.postSuggestTitle(ref)
-    const url = `${APP.baseURL}movie/${id}/recommendations?api_key=${APP.apiKey}&ref=${ref}`;
+    const url = `${APP.baseURL}movie/${id}/recommendations?api_key=${APP.apiKey}&ref=${ref}`
     const response = await fetch(url)
 
-      try {
-        if (response.ok) {
-          let suggestResults = {
-              id, 
-              results: await response.json()
-          }
-          return APP.saveSuggest(suggestResults)
+    try {
+      if (response.ok) {
+        let suggestResults = {
+          id,
+          results: await response.json(),
         }
-      } catch (error) {
-        console.warn('Could not fetch movies', error)
+        return APP.saveSuggest(suggestResults)
       }
+    } catch (error) {
+      console.warn('Could not fetch movies', error)
+    }
   },
 
-  saveResults (movieResults) {
+  saveResults(movieResults) {
     let transaction = APP.makeTransaction('movieStore', 'readwrite')
     transaction.oncomplete = () => {
       let movies = movieResults.results
-      APP.buildList(movies) 
+      APP.buildList(movies)
     }
 
     let store = transaction.objectStore('movieStore')
@@ -237,7 +244,10 @@ const APP = {
       console.log('Successfully added movie results to movieStore', event)
     }
     request.onerror = (error) => {
-      console.log('Something went wrong in adding movie results to movieStore', error)
+      console.log(
+        'Something went wrong in adding movie results to movieStore',
+        error
+      )
     }
   },
 
@@ -245,7 +255,7 @@ const APP = {
     let transaction = APP.makeTransaction('suggestStore', 'readwrite')
     transaction.oncomplete = () => {
       let movies = suggestResults.results
-      APP.buildList(movies) 
+      APP.buildList(movies)
     }
 
     let store = transaction.objectStore('suggestStore')
@@ -255,30 +265,33 @@ const APP = {
       console.log('Successfully added suggested movies to suggestStore', event)
     }
     request.onerror = (error) => {
-      console.log('Something went wrong in adding suggested to movieStore', error)
+      console.log(
+        'Something went wrong in adding suggested to movieStore',
+        error
+      )
     }
   },
 
   buildList(movies) {
-    let params = new URL(document.location).searchParams;
-    let keyword = params.get('keyword');
+    let params = new URL(document.location).searchParams
+    let keyword = params.get('keyword')
     APP.postResultsTitle(keyword)
-    
+
     let searchQuery = new URL(document.location).searchParams
     let ref = searchQuery.get('ref')
     APP.postSuggestTitle(ref)
 
     let container = document.querySelector('.movies')
     container.innerHTML = movies.results
-    .map ( movie => {
-      let img = './img/icon-512x512.png';
-      if (movie.poster_path != null) {
-        img = APP.imgURL + 'w500/' + movie.poster_path;
-      } else {
-        img = APP.noImgUrl
-      }
-      
-      return `<div class=" movieCard card hoverable medium" data-id="${movie.id}">
+      .map((movie) => {
+        let img = './img/icon-512x512.png'
+        if (movie.poster_path != null) {
+          img = APP.imgURL + 'w500/' + movie.poster_path
+        } else {
+          img = APP.noImgUrl
+        }
+
+        return `<div class=" movieCard card hoverable medium" data-id="${movie.id}">
       <div class="card-image">
         <img src="${img}" alt="movie poster" class="notmaterialboxed"/>
         </div>
@@ -295,64 +308,66 @@ const APP = {
         <a href="#" class="find-suggested teal-text text-accent-4">See Recommended Movies<i class="material-icons right">theaters</i></a>
       </div>
     </div>`
-    }).join('\n') 
+      })
+      .join('\n')
   },
 
   openDB() {
     let dbOpenRequest = indexedDB.open('movieDB', APP.dbVersion)
-    
+
     dbOpenRequest.addEventListener('upgradeneeded', (event) => {
       APP.db = event.target.result
 
       let oldVersion = event.oldVersion
       let newVersion = event.newVersion || APP.db.version
-      console.log(`movieDB Version Update: From version ${oldVersion} to version ${newVersion}`)
-      
-      if( !APP.db.objectStoreNames.contains('movieStore' && 'suggestStore')) { 
-        APP.moviedbStore = APP.db.createObjectStore('movieStore', { keyPath: 'keyword' })
-        APP.db.createObjectStore('suggestStore', { keyPath: 'id'})
+      console.log(
+        `movieDB Version Update: From version ${oldVersion} to version ${newVersion}`
+      )
+
+      if (!APP.db.objectStoreNames.contains('movieStore' && 'suggestStore')) {
+        APP.moviedbStore = APP.db.createObjectStore('movieStore', {
+          keyPath: 'keyword',
+        })
+        APP.db.createObjectStore('suggestStore', { keyPath: 'id' })
       }
     })
-    
+
     dbOpenRequest.addEventListener('success', (event) => {
       APP.db = event.target.result
       APP.pageLoaded()
     })
-    
+
     dbOpenRequest.addEventListener('error', (error) => {
-      console.warn('Something went wrong with opening movieDB stores:',error)
+      console.warn('Something went wrong with opening movieDB stores:', error)
     })
   },
 
-  makeTransaction (storeName, mode) {
+  makeTransaction(storeName, mode) {
     let transaction = APP.db.transaction(storeName, mode)
     transaction.onerror = (error) => {
       console.log(error)
     }
     return transaction
   },
-  
+
   postResultsTitle(keyword) {
-    let keywordSpan = document.querySelector('.ref-keyword');
+    let keywordSpan = document.querySelector('.ref-keyword')
     if (keyword && keywordSpan) {
-      return keywordSpan.textContent = keyword;
+      return (keywordSpan.textContent = keyword)
     }
   },
-  
+
   postSuggestTitle(ref) {
-    let suggestSpan = document.querySelector('.ref-keyword');
+    let suggestSpan = document.querySelector('.ref-keyword')
     if (ref && suggestSpan) {
-      suggestSpan.textContent = ref;
+      suggestSpan.textContent = ref
     }
   },
 
-  clearForm (event) {
-    if (event) event.preventDefault() 
-    document.getElementById('searchForm').reset() 
-  }
-
+  clearForm(event) {
+    if (event) event.preventDefault()
+    document.getElementById('searchForm').reset()
+  },
 }
 
 document.addEventListener('DOMContentLoaded', APP.init)
-
-
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,12 +1,11 @@
-
-const DYNAMIC_CACHE = "dynamic-3"
-const STATIC_CACHE = "static-v3"
+const DYNAMIC_CACHE = 'dynamic-3'
+const STATIC_CACHE = 'static-v3'
 const OFFLINE_URL = '/404.html'
 const maxCacheSize = 50
 
 const STATIC_ASSETS = [
   '/',
-  '/home.html', 
+  '/home.html',
   '/404.html',
   '/css/app.css',
   '/css/materialize.min.css',
@@ -25,95 +24,92 @@ const STATIC_ASSETS = [
   '/img/gr-stocks-q8P8YoR6erg-unsplash.jpg',
   '/manifest.json',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
-  'https://fonts.gstatic.com/s/materialicons/v82/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+  'https://fonts.gstatic.com/s/materialicons/v82/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
 ]
 
-function limitCacheSize(cacheName, maxCacheSize) {
-  caches.open(cacheName)
-  .then(cache => { cache.keys()
-    .then(keys => {
+function limitCacheSize (cacheName, maxCacheSize) {
+  caches.open(cacheName).then((cache) => {
+    cache.keys().then((keys) => {
       if (keys.length > maxCacheSize) {
-        cache.delete(keys[0])
-        .then(limitCacheSize(cacheName, maxCacheSize))
+        cache.delete(keys[0]).then(limitCacheSize(cacheName, maxCacheSize))
       }
     })
   })
 }
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     (async () => {
       const cache = await caches.open(STATIC_CACHE)
       console.log('Caching static assets')
-      await cache.addAll(STATIC_ASSETS) 
+      await cache.addAll(STATIC_ASSETS)
     })()
   )
   self.skipWaiting()
 })
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    (async() => {
+    (async () => {
       const keys = await caches.keys()
-      return Promise.all(keys
-        .filter(key => (key !== STATIC_CACHE && key !== DYNAMIC_CACHE)) 
-            .map(key => caches.delete(key))
-          )
+      return Promise.all(
+        keys
+          .filter((key) => key !== STATIC_CACHE && key !== DYNAMIC_CACHE)
+          .map((key) => caches.delete(key))
+      )
     })()
   )
-  self.clients.claim(); 
+  self.clients.claim()
 })
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   if (event.request.method === 'GET') {
-  event.respondWith(
-    (async () => {
-      const cachedResponse = await caches.match(event.request)
-      const dynamicCache = await caches.open(DYNAMIC_CACHE)
-      
-      if (cachedResponse) return cachedResponse
-    
-      try{
-        const networkResponse = await fetch(event.request)
-        dynamicCache.put(event.request, networkResponse.clone())
-        limitCacheSize(DYNAMIC_CACHE, maxCacheSize)
-        return networkResponse
-      } catch(error) {
-        console.log(error, "Returning offline page instead");
-        const cache = await caches.open(STATIC_CACHE);
-        const cachedResponse = await cache.match(OFFLINE_URL);
-        return cachedResponse;
-      }
-    })()
-  )}
+    event.respondWith(
+      (async () => {
+        const cachedResponse = await caches.match(event.request)
+        const dynamicCache = await caches.open(DYNAMIC_CACHE)
+
+        if (cachedResponse) return cachedResponse
+
+        try {
+          const networkResponse = await fetch(event.request)
+          dynamicCache.put(event.request, networkResponse.clone())
+          limitCacheSize(DYNAMIC_CACHE, maxCacheSize)
+          return networkResponse
+        } catch (error) {
+          console.log(error, 'Returning offline page instead')
+          const cache = await caches.open(STATIC_CACHE)
+          const cachedResponse = await cache.match(OFFLINE_URL)
+          return cachedResponse
+        }
+      })()
+    )
+  }
 })
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.mode === "navigate") {
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
     event.respondWith(
       (async () => {
         try {
-          const preloadResponse = await event.preloadResponse;
-          if (preloadResponse) { 
-            return preloadResponse;
+          const preloadResponse = await event.preloadResponse
+          if (preloadResponse) {
+            return preloadResponse
           }
 
-          const networkResponse = await fetch(event.request);
-          return networkResponse;
-
+          const networkResponse = await fetch(event.request)
+          return networkResponse
         } catch (error) {
-          console.log("Fetch failed; returning offline page instead.", error);
-          const cache = await caches.open(STATIC_CACHE);
-          const cachedResponse = await cache.match(OFFLINE_URL);
-          return cachedResponse;
+          console.log('Fetch failed; returning offline page instead.', error)
+          const cache = await caches.open(STATIC_CACHE)
+          const cachedResponse = await cache.match(OFFLINE_URL)
+          return cachedResponse
         }
       })()
-    );
+    )
   }
-});
+})
 
 self.addEventListener('message', ({ data }) => {
   console.log('Message from service worker', data)
 })
-
-
